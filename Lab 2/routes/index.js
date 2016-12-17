13e2c2ffb66220e51b92c51a29fc44323923c3b8
wadernik <zakaraya.ilia@gmail.com>
2016-12-17 03:39:17 +0300

    Page loading test

--- Lab 2/routes/index.js
+++ Lab 2/routes/index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var request = require('request');
 var querystring = require('querystring');
+var cheerio = require('cheerio');
 var cookieJar = request.jar();
 var pool = new http.Agent({keepAlive : true});
 
@@ -33,7 +34,7 @@ var authOptions = {
 
 
 
-function callbck(error, response, body) {
+function loginConsoleLog(error, response, body) {
     if (!error) {
         console.log("Header: " + JSON.stringify(response.headers));
         console.log("Body: " + JSON.stringify(response.body));
@@ -41,12 +42,20 @@ function callbck(error, response, body) {
     }
 }
 
-var getFirstPageOpts = {
+var getFirstPageOptions = {
     url : 'http://lageless-001-site1.htempurl.com/book/1',
-    jar : cookieJar
+    jar : cookieJar,
+    agent : pool,
+    headers : {
+
+        'Host' : 'lageless-001-site1.htempurl.com',
+        'Origin' : 'http://lageless-001-site1.htempurl.com',
+        'Referer' : 'http://lageless-001-site1.htempurl.com',
+        'User-Agent' : 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+    }
 };
 
-function callback(error, response, body) {
+function firstPageConsoleLog(error, response, body) {
     if (!error && response.statusCode == 200) {
         console.log("Body: " + body);
         console.log("Cookie: " + JSON.stringify(cookieJar));
@@ -54,5 +63,10 @@ function callback(error, response, body) {
     }
 }
 
-request.post(authOptions, callbck);
-request(getFirstPageOpts, callback);
+function getParsed(error, response, body){
+    var $ = cheerio.load(body);
+    console.log($);
+}
+
+request.post(authOptions, loginConsoleLog);
+request(getFirstPageOptions, firstPageConsoleLog);
