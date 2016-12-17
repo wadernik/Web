f6e4d783c30b9356934ebb0c38ebd28b4dc671c2
wadernik <zakaraya.ilia@gmail.com>
2016-12-17 17:26:33 +0300

    Request for dropping comment

--- Lab 2/routes/index.js
+++ Lab 2/routes/index.js
@@ -1,29 +1,35 @@
 var http = require('http');
 var request = require('request');
 var querystring = require('querystring');
-var cheerio = require('cheerio');
 var cookieJar = request.jar();
 var pool = new http.Agent({keepAlive : true});
 
-var form = {
+var authForm = {
     'Login' : 'mylgor',
     'Password' : 'qwerty'
 };
 
-var formData = querystring.stringify(form);
-var contentLength = formData.length;
+var postForm = {
+    'book.BookID' : '1',
+    'tempComment' : 'This is node js test message'
+};
+
+var authFormData = querystring.stringify(authForm);
+var postFormData = querystring.stringify(postForm);
+
+/* Options for request */
 
 var authOptions = {
     url : 'http://lageless-001-site1.htempurl.com/Login/Authen/',
     agent : pool,
-    body : formData,
+    body : authFormData,
     jar: cookieJar,
     headers : {
         'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Encoding' : 'gzip, deflate',
         'Accept-Language' : 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
         'Cache-Control' : 'max-age=0',
-        'Content-Length' : contentLength,
+        'Content-Length' : authFormData.length,
         'Content-Type' : 'application/x-www-form-urlencoded',
         'Host' : 'lageless-001-site1.htempurl.com',
         'Origin' : 'http://lageless-001-site1.htempurl.com',
@@ -32,41 +38,55 @@ var authOptions = {
     }
 };
 
-
-
-function loginConsoleLog(error, response, body) {
-    if (!error) {
-        console.log("Header: " + JSON.stringify(response.headers));
-        console.log("Body: " + JSON.stringify(response.body));
-        console.log("Jar: " + JSON.stringify(cookieJar));
-    }
-}
-
 var getFirstPageOptions = {
-    url : 'http://lageless-001-site1.htempurl.com/book/1',
+    url : 'http://lageless-001-site1.htempurl.com',
     jar : cookieJar,
     agent : pool,
-    headers : {
+    gzip : true
+};
 
+var postCommentOptions = {
+    url : 'http://lageless-001-site1.htempurl.com/BDetail/PostComment',
+    agent : pool,
+    formData : postFormData,
+    jar : cookieJar,
+    headers : {
+        'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding' : 'gzip, deflate',
+        'Accept-Language' : 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+        'Cache-Control' : 'max-age=0',
+        'Content-Length' : postFormData.length,
+        'Content-Type' : 'application/x-www-form-urlencoded',
         'Host' : 'lageless-001-site1.htempurl.com',
         'Origin' : 'http://lageless-001-site1.htempurl.com',
-        'Referer' : 'http://lageless-001-site1.htempurl.com',
+        'Referer' : 'http://lageless-001-site1.htempurl.com/Login/Authen',
         'User-Agent' : 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
     }
 };
 
+/* Loggers */
+
+function loginConsoleLog(error, response, body) {
+    if (!error) {
+        console.log("Header: " + JSON.stringify(response.headers));
+        console.log("Body: " + body);
+    }
+}
+
 function firstPageConsoleLog(error, response, body) {
     if (!error && response.statusCode == 200) {
         console.log("Body: " + body);
-        console.log("Cookie: " + JSON.stringify(cookieJar));
         console.log("Status: " + response.statusCode);
     }
 }
 
-function getParsed(error, response, body){
-    var $ = cheerio.load(body);
-    console.log($);
+function callback(error, response, body) {
+    console.log("Header: " + JSON.stringify(response.headers));
+    console.log("Response: " + response.statusCode);
 }
 
-request.post(authOptions, loginConsoleLog);
-request(getFirstPageOptions, firstPageConsoleLog);
+/* Requests */
+
+request.post(authOptions);
+//request(getFirstPageOptions, firstPageConsoleLog);
+request.post(postCommentOptions);
