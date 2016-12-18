99df427478ba0b4628627a45365704485c72dc2f
wadernik <zakaraya.ilia@gmail.com>
2016-12-19 00:59:23 +0300

    Lab 3 with auth

---
+++ Lab 3/Chat/Chat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chat.Startup))]
+namespace Chat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
