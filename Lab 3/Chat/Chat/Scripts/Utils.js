63773911385124344e2e6fda4ef9ec7bf919e30b
wadernik <zakaraya.ilia@gmail.com>
2016-12-19 20:59:13 +0300

    SignalR based chatter

---
+++ Lab 3/Chat/Chat/Scripts/Utils.js
@@ -0,0 +1,45 @@
+﻿$(function () {
+    var chat = $.connection.chatHub;
+    chat.client.addMessage = function (name, message) {
+        $('#chatroom').append('<p><b>' + htmlEncode(name)
+        + '</b>: ' + htmlEncode(message) + '</p>');
+    };
+
+    chat.client.onConnected = function (id, userName, allUsers) {
+
+        $('#hdId').val(id);
+
+        for (i = 0; i < allUsers.length; i++) {
+            AddUser(allUsers[i].ConnectionId, allUsers[i].Name);
+        }
+    }
+
+    chat.client.onNewUserConnected = function (id, userName) {
+        AddUser(id, userName);
+    }
+
+    chat.client.onUserDisconnected = function (id, userName) {
+        
+        $('#' + id).remove();
+    }
+
+    $.connection.hub.start().done(function () {
+        alert($('#username').val());
+        chat.server.connect($('#username').val());
+        $('#sendmessage').click(function () {
+            chat.server.send($('#username').val(), $('#message').val());
+            $('#message').val('');
+            //chat.server.connect($('#username').val());
+        });
+    });
+});
+
+function htmlEncode(value) {
+    var encodedValue = $('<div />').text(value).html();
+    return encodedValue;
+}
+
+function AddUser(id, name) {
+    var userId = $('#hdId').val();
+    $("#chatusers").append('<p id="' + id + '"><b>' + name + '</b></p>');
+}
